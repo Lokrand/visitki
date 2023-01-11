bd220ca0e2b79b9ff8a262845489d4b02b209252
kglidiya <betysmith@yandex.ru>
2023-01-11 13:43:59 +0300

    feat color costants added

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,11 +4,15 @@ import { Login } from "../../pages/Login/Login";
 import { Main } from "../../pages/Main/Main";
 import { Footer } from "../Footer/Footer";
 import { Header } from "../Header/Header";
+import Input from "../Input/Input";
 
 export const App = () => {
   return (
     <>
       <Header />
+      <Input
+      type="text"
+      text="jjj"/>
       <Routes>
         <Route path='/' element={<Main />} />
         <Route path='/login' element={<Login />} />
