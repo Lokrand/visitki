d5c3249f7e57a05b3b461a699d391df404796f8e
Dima <Uchiha132132qqq@gmail.com>
2023-01-11 12:08:09 +0300

    start kit

---
+++ src/components/App/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Login } from "../../pages/Login/Login";
+import { Main } from "../../pages/Main/Main";
+import { Footer } from "../Footer/Footer";
+import { Header } from "../Header/Header";
+
+export const App = () => {
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path='/' element={<Main />} />
+        <Route path='/login' element={<Login />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+};
---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.module.scss';
+export const Footer = () => {
+  return (
+    <>
+    <h1 className={styles.h}>The quick brown fox jumps over the lazy dog.</h1>
+    <p className={styles.p}>The quick brown fox jumps over the lazy dog.</p>
+    </>
+  )
+}
