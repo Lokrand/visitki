9991d1bebc7cf81e603fd6d8d225dfc56e9173da
Dima <Uchiha132132qqq@gmail.com>
2023-01-12 11:00:10 +0300

    feat: типизировал Loader #9

--- src/icons/Loader/Loader.tsx
+++ src/icons/Loader/Loader.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { FC } from "react";
 
-export const Loader = () => {
+export const Loader: FC = () => {
   return (
     <svg width='40' height='40' viewBox='0 0 40 40' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
