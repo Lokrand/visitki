9815349b063ffc7b62b7dc62e04c0c59472fd663
pomazkovanton <vitrumbeta@gmail.com>
2023-01-14 13:46:51 +0800

    refactor(ui): рефактор компонента с заголовками #21

--- src/components/UI/Title/Title.tsx
+++ src/components/UI/Title/Title.tsx
@@ -1,7 +1,7 @@
 import cx from "classnames";
-import React from "react";
+import { FC } from "react";
 
-import classes from "./title.module.scss";
+import styles from "./title.module.scss";
 
 type TSizeTitle = "h1" | "h2" | "h3" | "h4";
 
@@ -10,17 +10,17 @@ interface ITitleProps {
   children: React.ReactNode;
 }
 
-export const Title: React.FC<ITitleProps> = ({ size, children }) => {
+export const Title: FC<ITitleProps> = ({ size, children }) => {
   const switchTitle = (size: TSizeTitle): React.ReactElement => {
     switch (size) {
       case "h1":
-        return <h1 className={cx(classes.title, classes.titleH1)}>{children}</h1>;
+        return <h1 className={cx(styles.title, styles.titleH1)}>{children}</h1>;
       case "h2":
-        return <h2 className={cx(classes.title, classes.titleH2)}>{children}</h2>;
+        return <h2 className={cx(styles.title, styles.titleH2)}>{children}</h2>;
       case "h3":
-        return <h3 className={cx(classes.title, classes.titleH3)}>{children}</h3>;
+        return <h3 className={cx(styles.title, styles.titleH3)}>{children}</h3>;
       case "h4":
-        return <h4 className={cx(classes.title, classes.titleH4)}>{children}</h4>;
+        return <h4 className={cx(styles.title, styles.titleH4)}>{children}</h4>;
     }
   };
   return switchTitle(size);
