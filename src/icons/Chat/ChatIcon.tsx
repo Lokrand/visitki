95e8d91c19fc12def205405b42f083816a32deb7
Dima <Uchiha132132qqq@gmail.com>
2023-01-15 22:16:19 +0300

    feat: добавил компонент чата #32

--- src/icons/Chat/ChatIcon.tsx
+++ src/icons/Chat/ChatIcon.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from "react";
 
-export const Chat: FC = () => {
+export const ChatIcon: FC = () => {
   return (
     <svg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'>
       <path
