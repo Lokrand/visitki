2c1a52ff97a334865106d9fbae29b9b5e02a0734
Daniel-il <ilyin@DESKTOP-D9MBSO0>
2023-01-17 22:34:52 +0300

    feat: добавить хэдер

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,28 @@
-import React, { FC } from "react";
+import { FC, useEffect } from "react";
 
-import styles from "./Header.module.scss";
+import headerStyles from "./Header.module.scss";
 
+import { useFetch } from "../../hook/useFetch";
+import { Logo } from "../../icons/Logo/Logo";
+import { getFullProfile } from "../../utils/api";
 export const Header: FC = () => {
-  return <div className={styles.header}>Header</div>;
+  useEffect(() => {
+    localStorage.setItem("userId", "abfccdaa23e0bd1c4448d2f3");
+  }, []);
+  const userId: string = localStorage.getItem("userId");
+  console.log(userId);
+  const { url, method } = getFullProfile(userId);
+  const { data, error, loading } = useFetch(url, method);
+
+  return (
+    <section className={headerStyles.header}>
+      <Logo className={headerStyles.header__logo} />
+      {data !== null && (
+        <div className={headerStyles.user}>
+          <img src={data.profile.photo} alt='аватар' className={headerStyles.user__avatar} />
+          <p className={headerStyles.user__name}>{data.profile.name}</p>
+        </div>
+      )}
+    </section>
+  );
 };
