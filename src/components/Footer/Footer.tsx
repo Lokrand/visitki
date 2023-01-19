2f0832d42f9b352cbb2765d82787cc34330d4401
pomazkovanton <vitrumbeta@gmail.com>
2023-01-19 13:49:43 +0800

    fix: внести поправки в  footer и header #41

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,11 +1,10 @@
 import styles from "./Footer.module.scss";
+
 export const Footer = () => {
   return (
-    <>
-      <section className={styles.footer}>
-        <p className={styles.footer__text}>© Визитки</p>
-        <p className={styles.footer__text_praktikum}>Яндекс Практикум</p>
-      </section>
-    </>
+    <footer className={styles.footer}>
+      <p className={styles.footer__text}>© Визитки</p>
+      <p className={styles.footer__text_praktikum}>Яндекс Практикум</p>
+    </footer>
   );
 };
