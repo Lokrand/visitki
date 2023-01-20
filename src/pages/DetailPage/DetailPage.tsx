66065b93a53535d89f54badcf361b0f3fcc4a5ac
pomazkovanton <vitrumbeta@gmail.com>
2023-01-20 15:29:25 +0800

    feat: добавить верстку для страницы с деталями #41

--- src/pages/DetailPage/DetailPage.tsx
+++ src/pages/DetailPage/DetailPage.tsx
@@ -2,8 +2,10 @@ import { FC } from "react";
 
 import { useParams } from "react-router-dom";
 
-import styles from "./DetailPage.module.scss";
+import styles from "./detailpage.module.scss";
 
+import { Chat } from "../../components/UI/Chat/Chat";
+import { Post } from "../../components/UI/Post";
 import { SocialLink } from "../../components/UI/SocialLink";
 import { Text } from "../../components/UI/Text";
 import { Title } from "../../components/UI/Title";
@@ -18,37 +20,82 @@ export const DetailPage: FC = () => {
 
   if (loading) return <h1>Идет загрузка данных...</h1>;
   if (error) return <h1>Пользователь не найден</h1>;
+
+  const openModal = () => {
+    console.log("Модальное окно открыто");
+  };
+
   return (
     <section className={styles.container}>
-      <div className={styles["top-wrapper"]}>
-        <div className={styles["social-wrapper"]}>
-          <Title size='xl'>{data?.profile.name}</Title>
-          <div className={styles.city}>
-            <Text>{data?.profile.city.name}</Text>
+      <div className={styles["main-wrapper"]}>
+        <div className={styles["top-wrapper"]}>
+          <div className={styles["social-wrapper"]}>
+            <Title size='xl'>{data?.profile.name}</Title>
+            <div className={styles.city}>
+              <Text>{data?.profile.city.name}</Text>
+            </div>
+            <ul className={styles["social-list"]}>
+              <li>
+                <SocialLink nameSocialNetwork='telegram' userName={data?.profile.telegram} />
+              </li>
+              <li>
+                <SocialLink nameSocialNetwork='github' userName={data?.profile.github} />
+              </li>
+            </ul>
           </div>
-          <ul className={styles["social-list"]}>
-            <li>
-              <SocialLink nameSocialNetwork='telegram' userName={data?.profile.telegram} />
-            </li>
-            <li>
-              <SocialLink nameSocialNetwork='github' userName={data?.profile.github} />
-            </li>
-          </ul>
+          <div className={styles["photo-wrapper"]}>
+            <div className={styles["photo-conteiner"]}>
+              <img
+                className={styles.photo}
+                src={data?.profile.photo}
+                alt={`Фотография пользователя ${data?.profile.name}`}
+              />
+              <div className={styles.counter}>
+                <Chat forImage={false} counter={data?.reactions || 0} onClick={openModal} />
+              </div>
+            </div>
+            {data?.profile.quote !== "" && (
+              <p className={styles.quote}>
+                <Marks />
+                <span>{data?.profile.quote}</span>
+              </p>
+            )}
+          </div>
         </div>
-        <div className={styles["photo-wrapper"]}>
-          <img
-            className={styles.photo}
-            src={data?.profile.photo}
-            alt={`Фотография пользователя ${data?.profile.name}`}
-          />
-          {data?.profile.quote === "" && (
-            <p className={styles.quote}>
-              <Marks />
-              <span>Делай, что должнои будь, что будет.</span>
-              {/* <span>{data?.profile.quote}</span> */}
-            </p>
-          )}
-        </div>
+        <ul className={styles["post-list"]}>
+          <li>
+            <Post
+              title='Увлечения'
+              text={data?.info.hobby.text || ""}
+              imgUrl={data?.info.hobby.image}
+              reactions={data?.info.hobby.reactions || 0}
+            />
+          </li>
+          <li>
+            <Post
+              title='Семья'
+              text={data?.info.status.text || ""}
+              imgUrl={data?.info.status.image}
+              reactions={data?.info.status.reactions || 0}
+            />
+          </li>
+          <li>
+            <Post
+              title='Cфера'
+              text={data?.info.job.text || ""}
+              imgUrl={data?.info.job.image}
+              reactions={data?.info.job.reactions || 0}
+            />
+          </li>
+          <li>
+            <Post
+              title='Учеба'
+              text={data?.info.edu.text || ""}
+              imgUrl={data?.info.edu.image}
+              reactions={data?.info.edu.reactions || 0}
+            />
+          </li>
+        </ul>
       </div>
     </section>
   );
