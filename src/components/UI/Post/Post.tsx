6371448f325083a62c613d04adb2c7a4b1bfaa9a
pomazkovanton <vitrumbeta@gmail.com>
2023-01-20 13:08:59 +0800

    feat: добавить коммпонент "пост" для страницы с делатями #41

---
+++ src/components/UI/Post/Post.tsx
@@ -0,0 +1,32 @@
+import { FC, useState } from "react";
+
+import styles from "./Post.module.scss";
+
+import { Chat } from "../Chat/Chat";
+import { Text } from "../Text";
+import { Title } from "../Title";
+
+interface IPostProps {
+  title: string;
+  imgUrl?: string;
+  text: string;
+  reactions: number;
+}
+
+export const Post: FC<IPostProps> = ({ title, imgUrl, text, reactions }) => {
+  const openModal = () => {
+    console.log("Модальное окно открыто!");
+  };
+
+  return (
+    <article className={styles.post}>
+      <div className={styles["post__border-top"]}></div>
+      <div className={styles.post__counter}>
+        <Chat forImage={false} counter={reactions} onClick={openModal} />
+      </div>
+      <Title size='m'>{title}</Title>
+      {imgUrl && <img src={imgUrl} alt={title} />}
+      <Text>{text}</Text>
+    </article>
+  );
+};
