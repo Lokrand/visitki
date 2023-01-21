10ebffaaefa5b99c0392f6dfb6000e2940e60ea7
SmokyDogg <golovkin47@gmail.comgit config --global user.email golovkin47@gmail.com>
2023-01-21 23:18:17 +0300

    feat: добавление admin и comment users

--- src/pages/AdminCommentPage/AdminCommentPage.tsx
+++ src/pages/AdminCommentPage/AdminCommentPage.tsx
@@ -2,6 +2,51 @@ import React, { FC } from "react";
 
 import styles from "./AdminCommentPage.module.scss";
 
+import Input from "../../components/Input/Input";
+
+import { Cross } from "../../icons/Cross/Cross";
+
+const CommentFrame = () => {
+  return (
+    <>
+      <div className={styles.frames}>
+        <p className={`${styles.frame}`}>1853</p>
+        <p className={`${styles.frame}`}>20.12.2022</p>
+        <p className={`${styles.frame}`}>Дмитрий Степанов</p>
+        <p className={`${styles.frame}`}>Виктория Листвина</p>
+        <p className={`${styles.frame}`}>из блока Увлечения</p>
+        <p className={`${styles.frame}`}>
+          Классные у тебя увлечения, я тоже играю в настолки, любимая игра — Эволюция. Люблю еще слушать музыку, ходить
+          в кино, общаться с друзьями. Ну и учиться в Практикуме.{" "}
+        </p>
+        <Cross />
+      </div>
+    </>
+  );
+};
+
 export const AdminCommentPage: FC = () => {
-  return <h1>Admin Coment Page</h1>;
+  return (
+    <>
+      <Input
+        label='Фильтровать'
+        inputName='AdminCommentPage'
+        placeholder='По имени или фамилии или почте или номеру когорты (введите любой из этих параметров)'
+        setValue='kek'
+        form='oue'
+      />
+      <div className={styles.main}>
+        <p className={styles.column}>Когорта</p>
+        <p className={styles.column}>Дата</p>
+        <p className={styles.column}>Отправитель</p>
+        <p className={styles.column}>Получатель</p>
+        <p className={styles.column}>Откуда комментарий</p>
+        <p className={styles.column}>Текст комментария</p>
+      </div>
+      <CommentFrame />
+      <CommentFrame />
+      <CommentFrame />
+      <CommentFrame />
+    </>
+  );
 };
--- src/pages/AdminUsersPage/AdminUsersPage.tsx
+++ src/pages/AdminUsersPage/AdminUsersPage.tsx
@@ -1,7 +1,61 @@
-import React, { FC } from "react";
+import { FC } from "react";
 
-import styles from "./adminUsersPage.module.scss";
+import styles from "./AdminUsersPage.module.scss";
+
+import Input from "../../components/Input/Input";
+
+import { Button } from "../../components/UI/Button";
+
+const StudentFrame: FC = () => {
+  return (
+    <div className={styles.frames}>
+      <input className={`${styles.cohort} ${styles.frame}`} />
+      <input className={`${styles.email} ${styles.frame}`} />
+      <input className={`${styles.student} ${styles.frame}`} />
+    </div>
+  );
+};
 
 export const AdminUsersPage: FC = () => {
-  return <h1>Admin Users Page</h1>;
+  const handlerClick = () => {
+    console.log("kek");
+  };
+
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.table}>
+          <Input
+            label='Фильтровать'
+            inputName='AdminUserPage'
+            setValue='лул'
+            placeholder='По имени или фамилии или почте или номеру когорты (введите любой из этих параметров)'
+            form='kek'
+          />
+          <div className={styles.main}>
+            <p className={styles.column}>Номер когорты</p>
+            <p className={styles.column}>E-mail</p>
+            <p className={styles.column}>Имя и фамилия студента</p>
+          </div>
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+          <StudentFrame />
+        </div>
+        <div className={styles.adder}>
+          <h3 className={styles.title}>Добавить студентов</h3>
+          <p className={styles.text}>
+            Чтобы добавить новых студентов, загрузите csv или xlsx файл: первая колонка должна содержать email
+            студентов, вторая колонка — номер когорты.
+          </p>
+          <Button size='l' children='Выберите файл' handlerClick={handlerClick} />
+        </div>
+      </div>
+    </>
+  );
 };
