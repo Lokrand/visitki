327b03ad28e97bf2538b73c626825caa3638ba5d
Dima <98553087+Lokrand@users.noreply.github.com>
2023-01-26 13:03:08 +0300

    Revert "feat(admin): anmin/users загрузка новых студентов, удаление #2"

--- src/components/FrameStudent/FrameStudent.tsx
+++ src/components/FrameStudent/FrameStudent.tsx
@@ -1,81 +1,45 @@
-import React, { Dispatch, FC, KeyboardEventHandler, ReactNode, SetStateAction } from "react";
+import React, { ChangeEventHandler, FC, useState } from "react";
 
 import { useNavigate } from "react-router";
 
 import styles from "./FrameStudent.module.scss";
 
 import { Delete } from "../../icons/Delete/Delete";
-import { useForm } from "../../hook/useForm";
+import { TUser } from "../../utils/types";
 
-import { TReqUserData, TUser } from "../../utils/types";
-
-type TStudentFrame = {
+type StudentFrame = {
   student: TUser;
-  setItemToHide: Dispatch<SetStateAction<string>>;
-  color?: string;
-  border?: string;
-  icon?: ReactNode;
 };
 
-export const StudentFrame: FC<TStudentFrame> = ({ student, setItemToHide, color, icon, border }) => {
+export const StudentFrame: FC<StudentFrame> = ({ student }) => {
   const navigate = useNavigate();
+  const [cohortValue, setCohortValue] = useState(student.cohort);
+  const [emailValue, setEmailValue] = useState(student.email);
+  const [nameValue, setNameValue] = useState("");
   const handleNameClick = () => {
     navigate(`detail/${student._id}`);
   };
-  const { values, handleChange, setValues } = useForm({
-    cohort: student.cohort,
-    email: student.email,
-    name: student.name,
-  });
-  const submitHandler = () => {
-    console.log(values);
+  const handleCohortInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setCohortValue(e.target.value);
   };
-  const onEnter: KeyboardEventHandler<HTMLInputElement> = (e) => {
-    if (e.key === "Enter") {
-      submitHandler();
-    }
+  const handleEmailInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmailValue(e.target.value);
   };
-
+  const handleNameInput: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setNameValue(e.target.value);
+  };
   return (
-    <form className={styles.frames} style={{ borderBottom: border }}>
-      <input
-        name='cohort'
-        value={values.cohort}
-        className={`${styles.cohort} ${styles.frame}`}
-        onChange={handleChange}
-        onKeyDown={onEnter}
-        style={{ color: color }}
-      />
-      <input
-        name='email'
-        value={values.email}
-        className={`${styles.email} ${styles.frame}`}
-        onChange={handleChange}
-        onKeyDown={onEnter}
-        style={{ color: color }}
-      />
+    <div className={`${styles.frames} `}>
+      <input value={cohortValue} className={`${styles.cohort} ${styles.frame}`} onChange={handleCohortInput} />
+      <input value={emailValue} className={`${styles.email} ${styles.frame}`} onChange={handleEmailInput} />
       {student.name ? (
         <p className={styles.student_text} onClick={handleNameClick}>
           {student.name}
         </p>
       ) : (
-        <input
-          name='name'
-          value={values.name}
-          className={`${styles.student} ${styles.frame}`}
-          onChange={handleChange}
-          onKeyDown={onEnter}
-          style={{ color: color }}
-        />
+        <input value={nameValue} className={`${styles.student} ${styles.frame}`} onChange={handleNameInput} />
       )}
-      <span
-        className={styles.icon}
-        onClick={() => {
-          setItemToHide(student._id);
-        }}
-      >
-        {icon}
-      </span>
-    </form>
+      <Delete />
+    </div>
   );
 };
