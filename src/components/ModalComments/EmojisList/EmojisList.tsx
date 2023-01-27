fd35982c79333476e574761f73f15fe0e430194d
Dima <Uchiha132132qqq@gmail.com>
2023-01-27 10:40:42 +0300

    feat: добавил клики на все эмоджи #86

--- src/components/ModalComments/EmojisList/EmojisList.tsx
+++ src/components/ModalComments/EmojisList/EmojisList.tsx
@@ -45,6 +45,24 @@ export const EmojisList: FC<IEmojisList> = ({ id, modalFor }) => {
 
   const [likeCount, setLikeCount] = useState(0);
   const [likeActive, setLikeActive] = useState(false);
+  const [disLikeCount, setDisLikeCount] = useState(0);
+  const [disLikeActive, setDisLikeActive] = useState(false);
+  const [helloCount, setHelloCount] = useState(0);
+  const [helloActive, setHelloActive] = useState(false);
+  const [smileCount, setSmileCount] = useState(0);
+  const [smileActive, setSmileActive] = useState(false);
+  const [sadCount, setSadCount] = useState(0);
+  const [sadActive, setSadActive] = useState(false);
+  const [laughCount, setLaughCount] = useState(0);
+  const [laughActive, setLaughActive] = useState(false);
+  const [irritationCount, setIrritationCount] = useState(0);
+  const [irritationActive, setIrritationActive] = useState(false);
+  const [shockCount, setShockCount] = useState(0);
+  const [shockActive, setShockActive] = useState(false);
+  const [loveCount, setLoveCount] = useState(0);
+  const [loveActive, setLoveActive] = useState(false);
+  const [blackHeartCount, setBlackHeartCount] = useState(0);
+  const [blackHeartActive, setBlackHeartActive] = useState(false);
 
   if (counter > 10) {
     countArr = splitEmojis(counter);
@@ -54,9 +72,21 @@ export const EmojisList: FC<IEmojisList> = ({ id, modalFor }) => {
       setLikeCount(counter);
     }
   }, [counter]);
-  if (countArr.length > 0) {
-    setLikeCount(countArr[0]);
-  }
+  useEffect(() => {
+    if (countArr.length > 0) {
+      setLikeCount(countArr[0]);
+      setDisLikeCount(countArr[1]);
+      setHelloCount(countArr[2]);
+      setSmileCount(countArr[3]);
+      setSadCount(countArr[4]);
+      setLaughCount(countArr[5]);
+      setIrritationCount(countArr[6]);
+      setShockCount(countArr[7]);
+      setLoveCount(countArr[8]);
+      setBlackHeartCount(countArr[9]);
+    }
+  }, [countArr]);
+
   const handleLikeClick = () => {
     setLikeCount(likeCount + 1);
     setLikeActive(true);
@@ -64,6 +94,78 @@ export const EmojisList: FC<IEmojisList> = ({ id, modalFor }) => {
   const handleActiveLikeClick = () => {
     setLikeCount(likeCount - 1);
     setLikeActive(false);
+  };
+  const handleDisLikeClick = () => {
+    setDisLikeCount(disLikeCount + 1);
+    setDisLikeActive(true);
+  };
+  const handleActiveDisLikeClick = () => {
+    setDisLikeCount(disLikeCount - 1);
+    setDisLikeActive(false);
+  };
+  const handleHelloClick = () => {
+    setHelloCount(helloCount + 1);
+    setHelloActive(true);
+  };
+  const handleActiveHelloClick = () => {
+    setHelloCount(helloCount - 1);
+    setHelloActive(false);
+  };
+  const handleSmileClick = () => {
+    setSmileCount(smileCount + 1);
+    setSmileActive(true);
+  };
+  const handleActiveSmileClick = () => {
+    setSmileCount(smileCount - 1);
+    setSmileActive(false);
+  };
+  const handleSadClick = () => {
+    setSadCount(sadCount + 1);
+    setSadActive(true);
+  };
+  const handleActiveSadClick = () => {
+    setSadCount(sadCount - 1);
+    setSadActive(false);
+  };
+  const handleLaughClick = () => {
+    setLaughCount(laughCount + 1);
+    setLaughActive(true);
+  };
+  const handleActiveLaughClick = () => {
+    setLaughCount(laughCount - 1);
+    setLaughActive(false);
+  };
+  const handleIrritationClick = () => {
+    setIrritationCount(irritationCount + 1);
+    setIrritationActive(true);
+  };
+  const handleActiveIrritationClick = () => {
+    setIrritationCount(irritationCount - 1);
+    setIrritationActive(false);
+  };
+  const handleShockClick = () => {
+    setShockCount(shockCount + 1);
+    setShockActive(true);
+  };
+  const handleActiveShockClick = () => {
+    setShockCount(shockCount - 1);
+    setShockActive(false);
+  };
+  const handleLoveClick = () => {
+    setLoveCount(loveCount + 1);
+    setLoveActive(true);
+  };
+  const handleActiveLoveClick = () => {
+    setLoveCount(loveCount - 1);
+    setLoveActive(false);
+  };
+  const handleBlackHeartClick = () => {
+    setBlackHeartCount(blackHeartCount + 1);
+    setBlackHeartActive(true);
+  };
+  const handleActiveBlackHeartClick = () => {
+    setBlackHeartCount(blackHeartCount - 1);
+    setBlackHeartActive(false);
   };
 
   return (
@@ -82,41 +184,122 @@ export const EmojisList: FC<IEmojisList> = ({ id, modalFor }) => {
         {likeCount > 0 ? <p className={styles.emojis__counter}>{likeCount}</p> : null}
       </div>
 
-      <div className={countArr[1] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          disLikeCount > 0
+            ? disLikeActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={disLikeActive ? handleActiveDisLikeClick : handleDisLikeClick}
+      >
         <img src={dislike} alt='Дизлайк' />
-        <p className={styles.emojis__counter}>{countArr[1]}</p>
+        {disLikeCount > 0 ? <p className={styles.emojis__counter}>{disLikeCount}</p> : null}
       </div>
-      <div className={countArr[2] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          helloCount > 0
+            ? helloActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={helloActive ? handleActiveHelloClick : handleHelloClick}
+      >
         <img src={hello} alt='Привет' />
-        <p className={styles.emojis__counter}>{countArr[2]}</p>
+        {helloCount > 0 ? <p className={styles.emojis__counter}>{helloCount}</p> : null}
       </div>
-      <div className={countArr[3] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          smileCount > 0
+            ? smileActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={smileActive ? handleActiveSmileClick : handleSmileClick}
+      >
         <img src={smile} alt='Улыбка' />
-        <p className={styles.emojis__counter}>{countArr[3]}</p>
+        {smileCount > 0 ? <p className={styles.emojis__counter}>{smileCount}</p> : null}
       </div>
-      <div className={countArr[4] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          sadCount > 0
+            ? sadActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={sadActive ? handleActiveSadClick : handleSadClick}
+      >
         <img src={sad} alt='Грусть' />
-        <p className={styles.emojis__counter}>{countArr[4]}</p>
+        {sadCount > 0 ? <p className={styles.emojis__counter}>{sadCount}</p> : null}
       </div>
-      <div className={countArr[5] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          laughCount > 0
+            ? laughActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={laughActive ? handleActiveLaughClick : handleLaughClick}
+      >
         <img src={laugh} alt='Смех' />
-        <p className={styles.emojis__counter}>{countArr[5]}</p>
+        {laughCount > 0 ? <p className={styles.emojis__counter}>{laughCount}</p> : null}
       </div>
-      <div className={countArr[6] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          irritationCount > 0
+            ? irritationActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={irritationActive ? handleActiveIrritationClick : handleIrritationClick}
+      >
         <img src={irritation} alt='Раздражение' />
-        <p className={styles.emojis__counter}>{countArr[6]}</p>
+        {irritationCount > 0 ? <p className={styles.emojis__counter}>{irritationCount}</p> : null}
       </div>
-      <div className={countArr[7] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          shockCount > 0
+            ? shockActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={shockActive ? handleActiveShockClick : handleShockClick}
+      >
         <img src={shock} alt='Удивление' />
-        <p className={styles.emojis__counter}>{countArr[7]}</p>
+        {shockCount > 0 ? <p className={styles.emojis__counter}>{shockCount}</p> : null}
       </div>
-      <div className={countArr[8] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          loveCount > 0
+            ? loveActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={loveActive ? handleActiveLoveClick : handleLoveClick}
+      >
         <img src={love} alt='Любовь' />
-        <p className={styles.emojis__counter}>{countArr[8]}</p>
+        {loveCount > 0 ? <p className={styles.emojis__counter}>{loveCount}</p> : null}
       </div>
-      <div className={countArr[9] ? `${styles.emojis__like} ${styles.emojis__like_active}` : styles.emojis__like}>
+      <div
+        className={
+          blackHeartCount > 0
+            ? blackHeartActive
+              ? `${styles.emojis__like} ${styles.emojis__like_selected}`
+              : `${styles.emojis__like} ${styles.emojis__like_active}`
+            : styles.emojis__like
+        }
+        onClick={blackHeartActive ? handleActiveBlackHeartClick : handleBlackHeartClick}
+      >
         <img src={blackHeart} alt='Чёрное сердце' />
-        <p className={styles.emojis__counter}>{countArr[9]}</p>
+        {blackHeartCount > 0 ? <p className={styles.emojis__counter}>{blackHeartCount}</p> : null}
       </div>
     </div>
   );
